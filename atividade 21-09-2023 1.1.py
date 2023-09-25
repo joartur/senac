a3c37da3251ab5f3da64522163067c3d9d7ca505
Joamerson Artunes <joartunes@gmail.com>
2023-09-25 17:08:35 -0300

    atividade 1

---
+++ atividade 21-09-2023 1.1.py
@@ -0,0 +1,20 @@
+valor1 = 1
+valor2 = 2
+valor3 = 3
+
+if (valor1 < valor2 and valor1 < valor3):
+    print("valor 1 é o menor valor")
+
+    ##########################################
+
+a = [True, False]
+b = [True, False]
+
+
+print("1   2   Resultado")
+
+
+for a in 1:
+    for b in 2:
+        resultado = a or b  # Operação lógica "OR" entre A e B
+        print(f"{a}   {b}   {resultado}")
